8f451500540eed8f7a7a4d7c934f25fc60c41631
cmwoodley <christopher.woodley@hotmail.co.uk>
2023-02-06 09:49:02 +0000

    Updates

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,7 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
+import torch
+from typing import List, Optional, Tuple, Union
 
 def clean_smiles(sd_file):
     smiles = []
@@ -15,3 +17,17 @@ def clean_smiles(sd_file):
         for i in range(len(smiles)):
             f.writelines(smiles[i])
     
+
+def scale_model_input(sample: torch.FloatTensor, timestep: Optional[int] = None) -> torch.FloatTensor:
+    """
+    Ensures interchangeability with schedulers that need to scale the denoising model input depending on the
+    current timestep.
+
+    Args:
+        sample (`torch.FloatTensor`): input sample
+        timestep (`int`, optional): current timestep
+
+    Returns:
+        `torch.FloatTensor`: scaled input sample
+    """
+    return sample
